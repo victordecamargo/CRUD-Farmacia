79b6a68452e99efb0cd070e4318b90ae74844595
Victor Rodrigues de Camargo <victorvii.camargo@hotmail.com>
2024-07-22 12:48:09 -0300

    Feat: Criacao CRUD Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,46 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "O atributo 'nome' é obrigatorio!")
+    @Size(min = 5, max = 100, message = "O atributo 'nome' deve conter no minimo 05 caracteres")
+        private String nome;
+
+    @NotBlank(message = "O atributo 'descricao' é obrigatório!")
+    @Size(min = 10, max = 1000, message = "O atributo 'texto' deve conter no mínimo 10 e no máximo 1000 caracteres")
+    private String descricao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.generation.farmacia.repository;
+
+import com.generation.farmacia.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    List<Categoria> findAllByNomeContainingIgnoreCase(String nome);
+}
